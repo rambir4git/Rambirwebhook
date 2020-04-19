a8b71a089109cf89a8cf203a027813d05660d742
Rambir <rambir.git@gmail.com>
2020-04-19 22:24:19 +0530

    adding express

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 express()
   .use(bodyParser.json())
-  .post('/', exports.dialogflowFirebaseFulfillment(request, response))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
- 
-exports.dialogflowFirebaseFulfillment (request, response){
+  .post('/', function(request, response){
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
@@ -716,4 +713,6 @@ exports.dialogflowFirebaseFulfillment (request, response){
   intentMap.set('level4',level4);
   intentMap.set('level5',level5);
   agent.handleRequest(intentMap);
-});
+})
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+ 
