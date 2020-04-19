d36799f6cf5e70777180d7125d8f0b1458f3c880
Rambir <rambir.git@gmail.com>
2020-04-19 22:33:08 +0530

    adding express

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 express()
   .use(bodyParser.json())
   .post('/', (request, response)=>{
-  //const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+  
+  const agent = new WebhookClient({ request, response });
  
   function aboutbot(agent){
     
