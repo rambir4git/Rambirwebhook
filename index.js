67241ef71df31389fc2a181b59e963d9ce15b912
Rambir <rambir.git@gmail.com>
2020-04-20 08:09:52 +0530

    adding detectIntent

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
-// for Dialogflow fulfillment library docs, samples, and to report issues
+// Rambir making this .js script for Mekbot running DialogFlow (api.ai)
+// 19 Apr 2020 (copied from work on serverless inline editor)
+// Need for this arose when there was need to call non-google i.e. mekvahan.api
+// Since firebase free function do not allow it, i moved out of inline editor
+
+// This script will run on the heroku or mekvahan server instance
+// This will provide us a custom webhook of fullfilment for our chatbot
+// peace _V_
+
 'use strict';
 const express = require('express')
 const path = require('path')
@@ -8,6 +15,8 @@ const PORT = process.env.PORT || 5000
 const functions = require('firebase-functions');
 const {WebhookClient, Platform} = require('dialogflow-fulfillment');
 const {Card, Suggestion, Payload} = require('dialogflow-fulfillment');
+const uuid = require('uuid');
+const dialogflow = require('dialogflow');
  
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
@@ -716,4 +725,67 @@ express()
   agent.handleRequest(intentMap);
 })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  
+  express()
+  .use(bodyParser.json())
+  .post('/mekbot', (data, context)=>{  
+    const projectId = 'mekbot-7a8c7';
+    const sessionId = uuid.v4();
+    const languageCode = 'en';
+    const query = data.question; 
+	const contexts = data.context;
+	
+	const auth = data.auth;
+	
+    const sessionClient = new dialogflow.SessionsClient();
+	
+    async function detectIntent(projectId, sessionId, query, contexts, languageCode) {
+		
+        // The path to identify the agent that owns the created intent.
+        const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+        // The text query request.
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    text: query,
+                    languageCode: languageCode,
+                },
+            },
+        };
+
+        if (contexts && contexts.length > 0) {
+            request.queryParams = {
+                contexts: contexts,
+            };
+        }
+
+        const responses = await sessionClient.detectIntent(request);
+        return responses[0];
+    }
+
+    async function executeQuery(projectId, sessionId, query, languageCode) {
+        try {
+            console.log(`Sending Query: ${query}`);
+            intentResponse = await detectIntent(
+                projectId,
+                sessionId,
+                query,
+                contexts,
+                languageCode
+            );
+            console.log('Detected intent');
+            console.log(`Fulfillment Text: ${intentResponse.queryResult.fulfillmentText}`);
+            return intentResponse.queryResult 
+			
+        } catch (error) {
+            console.log(error);
+            return "Error detecting Intent"   
+        }
+    }
+
+    return executeQuery(projectId, sessionId, query, languageCode); 
+})
+  .listen(PORT+1, () = console.log(`Listening on ${ PORT+1 }`));
  
